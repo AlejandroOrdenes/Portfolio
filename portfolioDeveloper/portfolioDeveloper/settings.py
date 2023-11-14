9c4ac5886af2d53209ec08a63247d7e895bf008f
AlejandroOrdenes <alejandro.ordenes.o@gmail.com>
2023-11-14 11:39:53 -0300

    staticfiles

--- portfolioDeveloper/portfolioDeveloper/settings.py
+++ portfolioDeveloper/portfolioDeveloper/settings.py
@@ -131,8 +131,8 @@ LANGUAGES = [
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'portfolioApp/static'),
 ]
