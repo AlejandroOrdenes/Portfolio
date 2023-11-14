845d67e13eb8440ee82236b39e8cabb928b345a1
AlejandroOrdenes <alejandro.ordenes.o@gmail.com>
2023-11-13 21:19:25 -0300

    .po translate

--- portfolioDeveloper/portfolioApp/views.py
+++ portfolioDeveloper/portfolioApp/views.py
@@ -3,8 +3,4 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your views here.
 def index(request):
-    p =  _("I'm a")
-    return render(request, 'index.html', {'text' : p})
-
-def textTraductions(request):
-    return render()
+    return render(request, 'index.html')
