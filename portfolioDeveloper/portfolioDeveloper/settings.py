489dc048627d585b8f59ebaa341ef95009380613
Alejandro Ordenes <alejandro.ordenes.o@gmail.com>
2025-10-03 10:24:38 -0400

    settings update

--- portfolioDeveloper/portfolioDeveloper/settings.py
+++ portfolioDeveloper/portfolioDeveloper/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 DATABASE_URL = config('DATABASE_URL')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 LOCALE_PATHS = [
