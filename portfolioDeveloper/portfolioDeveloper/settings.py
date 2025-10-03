09bc807b594c47c0c761796427d00c652614948d
Alejandro Ordenes <alejandro.ordenes.o@gmail.com>
2025-10-03 12:11:16 -0400

    settings update

--- portfolioDeveloper/portfolioDeveloper/settings.py
+++ portfolioDeveloper/portfolioDeveloper/settings.py
@@ -138,9 +138,12 @@ LANGUAGES = [
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = []   # déjalo vacío si no tienes carpetas extras
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolioApp/static'),
+]
+   # déjalo vacío si no tienes carpetas extras
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
